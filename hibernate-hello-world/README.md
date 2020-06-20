41db683904a3179f7d9ed7c0ab9151b0e8ca6eb1
Lokesh Gupta <howtodoinjava@gmail.com>
2020-06-21 02:38:45 +0530

    Create session factory

--- hibernate-hello-world/README.md
+++ hibernate-hello-world/README.md
@@ -1,2 +1,3 @@
 # Related Tutorials
 1. [Hibernate example – Step by step hello world example](https://howtodoinjava.com/hibernate/hibernate-hello-world-application/)
+2. [Hibernate - How to build SessionFactory](https://howtodoinjava.com/hibernate/hibarnate-build-sessionfactory/)
--- hibernate-hello-world/src/main/java/com/howtodoinjava/hibernate/test/HibernateUtil.java
+++ hibernate-hello-world/src/main/java/com/howtodoinjava/hibernate/test/HibernateUtil.java
@@ -1,32 +1,40 @@
 package com.howtodoinjava.hibernate.test;
 
-import java.io.File;
-
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
-	private static final SessionFactory sessionFactory = buildSessionFactory();
-	 
-    private static SessionFactory buildSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure(new File("hibernate.cgf.xml")).buildSessionFactory();
- 
-        }
-        catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
- 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
- 
-    public static void shutdown() {
-    	// Close caches and connection pools
-    	getSessionFactory().close();
-    }
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() 
+	{
+		try 
+		{
+			if (sessionFactory == null) 
+			{
+				StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+						.configure("hibernate.cfg.xml").build();
+				
+				Metadata metaData = new MetadataSources(standardRegistry)
+						.getMetadataBuilder()
+						.build();
+				
+				sessionFactory = metaData.getSessionFactoryBuilder().build();
+			}
+			return sessionFactory;
+		} catch (Throwable ex) {
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
 }
