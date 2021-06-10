a3b127240c5544465929b0cfe607ec486112ab7a
lokeshgupta1981 <howtodoinjava@gmail.com>
2021-06-11 00:05:44 +0530

    Initial Commit
    Initial Commit

---
+++ hibernate-examples/src/main/java/com/howtodoinjava/hibernate/demo/HibernateExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.hibernate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateExamplesApplication.class, args);
+	}
+
+}
