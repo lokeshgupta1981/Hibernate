77ce6586cdf17d6168566666ad08c63d4ddcea2d
lokeshgupta1981 <howtodoinjava@gmail.com>
2021-12-02 01:28:56 +0530

    Updates

--- hibernate-mappings/src/main/java/com/howtodoinjava/hibernate/onetoone/dto/foreignKeyAsso/AccountEntity.java
+++ hibernate-mappings/src/main/java/com/howtodoinjava/hibernate/onetoone/dto/foreignKeyAsso/AccountEntity.java
@@ -1,7 +1,6 @@
 package com.howtodoinjava.hibernate.onetoone.dto.foreignKeyAsso;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,45 +11,43 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity(name = "ForeignKeyAssAccountEntity")
-@Table(name = "ACCOUNT", uniqueConstraints = {
-		@UniqueConstraint(columnNames = "ID")})
-public class AccountEntity implements Serializable 
-{
+@Table(name = "ACCOUNT", uniqueConstraints = {@UniqueConstraint(columnNames = "ID")})
+public class AccountEntity implements Serializable {
 
-	private static final long serialVersionUID = -6790693372846798580L;
+  private static final long serialVersionUID = -6790693372846798580L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID", unique = true, nullable = false)
-	private Integer accountId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ID", unique = true, nullable = false)
+  private Integer accountId;
 
-	@OneToOne(mappedBy="account", optional=false)
-	private EmployeeEntity employee;
+  @OneToOne(mappedBy = "account", optional = false)
+  private EmployeeEntity employee;
 
-	@Column(name = "ACC_NUMBER", unique = true, nullable = false, length = 100)
-	private String accountNumber;
+  @Column(name = "ACC_NUMBER", unique = true, nullable = false, length = 100)
+  private String accountNumber;
 
-	public Integer getAccountId() {
-		return accountId;
-	}
+  public Integer getAccountId() {
+    return accountId;
+  }
 
-	public void setAccountId(Integer accountId) {
-		this.accountId = accountId;
-	}
-	
-	public EmployeeEntity getEmployee() {
-		return employee;
-	}
+  public void setAccountId(Integer accountId) {
+    this.accountId = accountId;
+  }
 
-	public void setEmployee(EmployeeEntity employee) {
-		this.employee = employee;
-	}
+  public EmployeeEntity getEmployee() {
+    return employee;
+  }
 
-	public String getAccountNumber() {
-		return accountNumber;
-	}
+  public void setEmployee(EmployeeEntity employee) {
+    this.employee = employee;
+  }
 
-	public void setAccountNumber(String accountNumber) {
-		this.accountNumber = accountNumber;
-	}
+  public String getAccountNumber() {
+    return accountNumber;
+  }
+
+  public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+  }
 }
